c91a4c98841368c5d7506745cce9384fd1b87bbf
Aida <aida.marat@gmail.com>
2024-06-30 15:47:15 -0500

    Added converter to a view

--- UnitCoverter/app/src/main/java/com/gamecodeschool/unitcoverter/ConversionUnit.kt
+++ UnitCoverter/app/src/main/java/com/gamecodeschool/unitcoverter/ConversionUnit.kt
@@ -1,34 +1,32 @@
 package com.gamecodeschool.unitcoverter
 
-enum class  ConversionUnit {
-    FEET,
-    METER,
-    FAHRENHEIT,
-    CELSIUS,
-    POUND,
-    KILOGRAM;
+enum class ConversionUnit(
+    val label: String,
+) {
+    FEET("ft"),
+    METER("m"),
+    FAHRENHEIT("℉"),
+    CELSIUS("℃"),
+    POUND("lbs"),
+    KILOGRAM("kg"),
+    ;
 
     companion object {
-        fun getConversionUnitsFor(unit: ConversionUnit): Array<ConversionUnit> {
-            when(unit) {
-                FEET ->  return arrayOf(ConversionUnit.METER)
-                METER ->  return arrayOf(ConversionUnit.FEET)
-                FAHRENHEIT ->  return arrayOf(ConversionUnit.CELSIUS)
-                CELSIUS ->  return arrayOf(ConversionUnit.FAHRENHEIT)
-                POUND -> return arrayOf(ConversionUnit.KILOGRAM)
-                KILOGRAM ->  return arrayOf(ConversionUnit.POUND)
+        fun getConversionUnitsFor(unit: ConversionUnit): Array<ConversionUnit> =
+            when (unit) {
+                FEET -> arrayOf(METER)
+                METER -> arrayOf(FEET)
+                FAHRENHEIT -> arrayOf(CELSIUS)
+                CELSIUS -> arrayOf(FAHRENHEIT)
+                POUND -> arrayOf(KILOGRAM)
+                KILOGRAM -> arrayOf(POUND)
             }
-        }
-        fun getLabelForUnit(unit: ConversionUnit): String {
-            when(unit) {
-                FEET -> return "ft"
-                METER -> return "m"
-                FAHRENHEIT ->  return "℉"
-                CELSIUS ->  return "℃"
-                POUND ->  return "lbs"
-                KILOGRAM ->  return "kg"
-            }
-        }
-    }
 
+        fun getLabelForUnit(unit: ConversionUnit): String = unit.label
+
+        fun from(findValue: String): ConversionUnit =
+            ConversionUnit.entries.first {
+                it.label == findValue
+            }
+    }
 }
--- UnitCoverter/app/src/main/java/com/gamecodeschool/unitcoverter/MainActivity.kt
+++ UnitCoverter/app/src/main/java/com/gamecodeschool/unitcoverter/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.gamecodeschool.unitcoverter
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var valueTextView: TextView
+    private lateinit var fromUnitView: Spinner
+    private lateinit var toUnitView: Spinner
+    private lateinit var resultView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,70 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        valueTextView = findViewById(R.id.valueTextView)
+        fromUnitView = findViewById(R.id.fromUnitSpinner)
+        toUnitView = findViewById(R.id.toUnitView)
+        resultView = findViewById(R.id.resultView)
+        setUpFromUnitOptions()
+
+        fromUnitView.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long,
+                ) {
+                    val fromUnit = ConversionUnit.from(parent?.selectedItem.toString())
+                    setUpToUnitOptions(fromUnit)
+                    calculate()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+            }
+        toUnitView.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long,
+                ) {
+                    calculate()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+            }
+
+        valueTextView.doOnTextChanged { _, _, _, _ -> calculate() }
+    }
+
+    private fun setUpFromUnitOptions() {
+        val list = ArrayList<String>()
+        ConversionUnit.entries.forEach {
+            val label = ConversionUnit.getLabelForUnit(it)
+            list.add(label)
+        }
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list)
+        fromUnitView.adapter = adapter
+    }
+
+    fun setUpToUnitOptions(unit: ConversionUnit) {
+        val list = ArrayList<String>()
+        ConversionUnit.getConversionUnitsFor(unit).forEach {
+            val label = ConversionUnit.getLabelForUnit(it)
+            list.add(label)
+        }
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list)
+        toUnitView.adapter = adapter
+    }
+
+    fun calculate() {
+        val fromUnit = ConversionUnit.from(fromUnitView.selectedItem.toString())
+        val toUnit = ConversionUnit.from(toUnitView.selectedItem.toString())
+        val value = valueTextView.text.toString().toDouble()
+        resultView.text = Converter.convert(value, fromUnit, toUnit).toString() + toUnit.label
     }
 }
